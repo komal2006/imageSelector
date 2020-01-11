4f7fb39819931a7063f3e56ba2b1bb01287efa99
Komal2006 <komaldeepkr99@gmail.com>
2020-01-11 13:31:52 -0500

    images with buttons

--- imageSelector/ViewController.swift
+++ imageSelector/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var Array = [ UIImage(named:"img1"),
+    UIImage(named:"img2"),
+    UIImage(named:"img3") ,
+    UIImage(named:"img4") ,
+    UIImage(named:"img5") ,
+    UIImage(named:"img6"),
+    UIImage(named:"img7") ,
+    UIImage(named:"img8") ,
+    UIImage(named:"img9") ,
+    UIImage(named:"img10") ]
+    var index : Int = 0
+   
+    @IBAction func btnFirst(_ sender: Any) {
+    }
+    
+    @IBAction func btnLast(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPrev(_ sender: UIButton) {
+        if(index > 0 ){
+                      index = index - 0
+
+                      let img =  Array[index]
+                      imageView.image = img
+    }
+    }
+    @IBAction func btnNext(_ sender: UIButton)
+    {
+        
+        if(index < Array.count){
+           // index = index + 1
+
+               let img =  Array[index]
+               imageView.image = img
+            index = index + 1
+        
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
+
 
